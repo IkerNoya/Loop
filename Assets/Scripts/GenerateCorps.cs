1e285c6df65a8d7849d2c60c7f9dcdb2dfb9eecd
theixgamer <theofritaku@gmail.com>
2020-10-03 00:15:52 -0300

    genera a nisman uwu

--- Assets/Scripts/GenerateCorps.cs
+++ Assets/Scripts/GenerateCorps.cs
@@ -6,32 +6,54 @@ public class GenerateCorps : MonoBehaviour
     // Start is called before the first frame update
     class ParentClass
     {
-        public GameObject levelsObjects;
+        public GameObject parentObject;
         public string nameLevel;
     }
 
-    [SerializeField] private PlayerController PlayerReference;
+    [SerializeField] private Character character;
     public GameObject objectCorpGenerate;
-    private ParentClass[] parentClasses;
+    private ParentClass parentClasses;
     // Update is called once per frame
 
     private void Start()
     {
         GameObject[] parents = GameObject.FindGameObjectsWithTag("Level");
-        parentClasses = new ParentClass[parents.Length];
-
-        for (int i = 0; i < parentClasses.Length; i++)
+        parentClasses = new ParentClass();
+        GameObject currentParent = null;
+        int index = 0;
+        for (int i = 0; i < parents.Length; i++)
         {
-            parentClasses[i].nameLevel = "Level" + (i + 1);
+            if (parents[i].activeSelf)
+            {
+                currentParent = parents[i];
+                index = i;
+                i = parents.Length;
+            }
         }
+        parentClasses.parentObject = currentParent;
+        parentClasses.nameLevel = "Level" + index;
     }
     void Update()
     {
-        
+        CheckGenerateCorp();
+        //BORRAR LUEGO DEL TESTEO
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            if (character == null) return;
+            character.SetHP(0);
+        }
     }
 
     public void CheckGenerateCorp()
     {
-        //Instantiate(objectCorpGenerate,)
+        if (character == null) return;
+
+        if (character.GetHP() <= 0)
+            GenerateCorp();
+    }
+    public void GenerateCorp()
+    {
+        Instantiate(objectCorpGenerate, transform.position, Quaternion.identity, parentClasses.parentObject.transform);
+        Destroy(character.gameObject);
     }
 }
