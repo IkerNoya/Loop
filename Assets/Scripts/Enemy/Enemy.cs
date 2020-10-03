85aaa25dda0580b7b047ab2bc6f7735ad1fbf61c
theixgamer <theofritaku@gmail.com>
2020-10-03 12:21:23 -0300

    PathFinding Enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,8 +6,8 @@ public class Enemy : Character
 {
     [SerializeField] protected float speed;
     [SerializeField] protected float damage;
-    [SerializeField] protected bool usePatrol;
-    [SerializeField] Transform waypointsList;
+    [SerializeField] protected bool usePatrullaje;
+    [SerializeField] Transform waypointsPatrullaje;
     protected FSM fsm;
 
     protected virtual void Attack() { }
