d4a2ad7c71cfa6de1903cf65c869fd0aa165c91a
Iker <ikernoya4@gmail.com>
2020-10-03 01:31:16 -0300

    Imported sprite package

--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class Weapons : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
+    [SerializeField] float timeToShoot;
+    float timer = 0;
+    bool canShoot = true;
     public void ShootSubmachineGun()
     {
-       if (bullet != null)
-           Instantiate(bullet, transform.position, Quaternion.identity);
-        
+        if (canShoot)
+        {
+            if (bullet != null)
+                Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+        canShoot = false;
+        timer += Time.deltaTime;
+        if(timer>= timeToShoot)
+        {
+            canShoot = true;
+        }
     }
 }
