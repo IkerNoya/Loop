0a91912bedb85e17d864f2060a02ff1e4bdb34ca
theixgamer <theofritaku@gmail.com>
2020-10-03 13:06:56 -0300

    Ferchu UwU

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Pathfinding;
 public class Enemy : Character
 {
     [SerializeField] protected float speed;
     [SerializeField] protected bool usePatrullaje;
     [SerializeField] Transform waypointsPatrullaje;
-    
+    private AIDestinationSetter aiPathDestination;
     protected FSM fsm;
-
+    private Transform currentTarget;
+    private PlayerController[] targets; 
     private void Awake()
     {
-        
+        aiPathDestination = GetComponent<AIDestinationSetter>();
+        targets = FindObjectsOfType<PlayerController>();
+    }
+
+    public void CheckCurrentTarget()
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+
+        }
     }
     protected virtual void Attack() { }
 }
