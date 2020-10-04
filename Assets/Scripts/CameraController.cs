6f83666f690cee8476ad92e67a1d7f200af17c32
fermingimpel <fermingimpel@gmail.com>
2020-10-04 02:08:17 -0300

    Cambiado fondo

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] Transform player;
+    void LateUpdate() {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
