fcdfd4a8e0eea5b0696a3a5017c8c8d924859480
Iker <ikernoya4@gmail.com>
2020-10-04 02:25:41 -0300

    Fixed camera shake

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     [SerializeField] Transform player;
+    CameraShake shake;
+    private void Start()
+    {
+        shake = GetComponent<CameraShake>();
+    }
     void LateUpdate() {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z) + shake.localPos;
     }
 }
--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-   public IEnumerator Shake(float duration, float magnitude)
+    public Vector3 localPos;
+    public IEnumerator Shake(float duration, float magnitude)
     {
-        Vector3 originalPos = transform.localPosition;
+        Vector3 originalPos = transform.position;
         float elapsed = 0.0f;
         while (elapsed < duration)
         {
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
             transform.localPosition = new Vector3(x, y, originalPos.z);
+            localPos = transform.position;
             elapsed += Time.deltaTime;
             yield return null;
         }
         transform.localPosition = originalPos;
+        localPos = Vector2.zero;
+        
     }
 }
